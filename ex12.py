74226fa37abe22a7c3ff7b525dd2872ca9536d28
camilaalmeidaa <99203530+camilaalmeidaa@users.noreply.github.com>
2022-02-10 17:55:17 -0300

    Add files via upload

---
+++ ex12.py
@@ -0,0 +1,15 @@
+def tabuada():
+    
+    num = int(input("*Exercicio12* Digite um numero de 1 e 10: "))
+    
+    while(num < 1 or num > 10):
+        
+        print("Numero invalido. Digite novamente.")
+        num = int(input("Digite um numero de 0 e 10: "))
+        
+    print("Tabuada de",num,":\n")
+    
+    for i in range(1,11,1):
+        
+        result = num * i
+        print(num,"X",i,"=",result)
