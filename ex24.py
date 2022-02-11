1cea5c19e1999fdf66f3f73addb32f4efab7f801
camilaalmeidaa <99203530+camilaalmeidaa@users.noreply.github.com>
2022-02-11 17:37:49 -0300

    Add files via upload

---
+++ ex24.py
@@ -0,0 +1,14 @@
+def media_notas(lista):
+    
+    soma = 0
+    
+    for i in range(len(lista)):
+        
+        soma = soma + lista[i]
+    
+    
+    media = soma / len(lista)
+    
+    print("Media:",media,"\n")
+        
+        
