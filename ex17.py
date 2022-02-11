4e50246d2d6ce801ecc7e4e96071bd4b530d9a52
camilaalmeidaa <99203530+camilaalmeidaa@users.noreply.github.com>
2022-02-11 14:58:57 -0300

    Add files via upload

---
+++ ex17.py
@@ -0,0 +1,15 @@
+def fatorial():
+    
+    num = int(input("*Exercicio17* Digite o numero que deseja saber o fatorial: "))
+    
+    fat = 1
+    count = 1
+
+    while count <= num: 
+        
+        fat = fat * count  
+        count = count + 1  
+
+    print(fat)
+
+
