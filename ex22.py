c85e285fe8423e22c2dcf4378295000c3c0c4bf8
camilaalmeidaa <99203530+camilaalmeidaa@users.noreply.github.com>
2022-02-11 15:09:53 -0300

    Add files via upload

---
+++ ex22.py
@@ -0,0 +1,22 @@
+def primo2():
+    
+    num = int(input("*Exercicio21* Digite um numero inteiro: "))
+    
+    nao_primo = 0
+  
+    
+    for i in range(2,num-1,1):
+        
+        if(num % i == 0):
+            
+            nao_primo = 1
+           
+            print("Divisivel por:",i)
+        
+    if(nao_primo == 1):
+        
+        print("Numero nao é primo.\n")
+    
+    else:
+        
+        print("Numero é primo\n")
