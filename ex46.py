55e6d81e28cc68b19d1b737f5ea126e43b55dfb5
camilaalmeidaa <99203530+camilaalmeidaa@users.noreply.github.com>
2022-02-15 15:40:48 -0300

    Add files via upload

---
+++ ex46.py
@@ -0,0 +1,41 @@
+def saltos():
+    
+    lista = []
+ 
+    nome = input("*Exercicio46* Digite o nome do atleta: ")
+    
+    for i in range(5):
+        
+        salto = float(input("*Exercicio46* Digite a distancia do salto: "))
+        
+        lista.append(salto)
+    
+    
+        
+    print("\nAtleta:",nome)
+            
+    for i in range(len(lista)):
+        
+        print("Salto",i+1,":",lista[i],"m")
+    
+   
+    
+    print("\nMelhor Salto:",max(lista),"m\nPior Salto:",min(lista))
+    
+    lista.remove(max(lista))
+    lista.remove(min(lista))
+    
+    media = sum(lista) / len(lista)
+    
+    print("Media dos demais saltos:",media)
+    
+    print("\nResultado final:\n")
+    print(nome,":",media,"m")
+        
+        
+       
+        
+        
+        
+    
+    
