fc020b0674417348e6556cea200ebc2153f42485
camilaalmeidaa <99203530+camilaalmeidaa@users.noreply.github.com>
2022-02-15 16:05:06 -0300

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,95 @@
+import ex1
+import ex2
+import ex3
+import ex4
+import ex5
+import ex6
+import ex7
+import ex8
+import ex9
+import ex10
+import ex11
+import ex12
+import ex13
+import ex14
+import ex15
+import ex16
+import ex17
+import ex18
+import ex19
+import ex20
+import ex21
+import ex22
+import ex24
+import ex25
+import ex26
+import ex27
+import ex28
+import ex29
+import ex30
+import ex31
+import ex32
+import ex33
+import ex34
+import ex35
+import ex36
+import ex37
+import ex38
+import ex39
+import ex40
+import ex41
+import ex42
+import ex43
+import ex44
+import ex45
+import ex46
+import ex47
+import ex48
+
+ex1.pede_nota()
+ex2.senha()
+ex3.valida()
+ex4.taxa_crescimento()
+ex5.taxa_crescimento2()
+ex6.um_a_vinte()
+ex7.maior()
+ex8.soma_media()
+ex9.impares()
+ex10.intervalo()
+ex11.intervalo2()
+ex12.tabuada()
+ex13.potencia()
+ex14.par_impar()
+ex15.fibonacci()
+ex16.fibonacci2()
+ex17.fatorial()
+ex18.n_numeros([1,2,3,4])
+ex19.n_numeros2([1,2,3,4,10000])
+ex20.fatorial2()
+ex21.primo()
+ex22.primo2()
+ex24.media_notas([1,2,3,4,5])
+ex25.media_idades([21,35,46,15,60,33,29])
+ex26.eleicao()
+ex27.media_alunos()
+ex28.cds()
+ex29.loja()
+ex30.loja2()
+ex31.caixa()
+ex32.fatorial3()
+ex33.temperaturas([23,33,21,35,15])
+ex34.primo3()
+ex35.primo4()
+ex36.tabuada2()
+ex37.academia()
+ex38.salario()
+ex39.alunos([(1,150),(2,147),(3,156),(4,157),(5,148),(6,152),(7,151),(8,144),(9,154),(10,155)])
+ex40.acidentes()
+ex41.juros()
+ex42.intervalos()
+ex43.pedido()
+ex44.votacao()
+ex45.nota_prova()
+ex46.saltos()
+ex47.ginasta()
+ex48.invertido()
